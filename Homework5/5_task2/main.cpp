aeccc8a28ff38a058ff342c7f187faacb4ac6e64
Ioinnes <kripakov.id@phystech.edu>
2022-03-22 16:21:32 +0300

    da task

--- Homework5/5_task2/main.cpp
+++ Homework5/5_task2/main.cpp
@@ -15,17 +15,13 @@ void check(std::string &data, std::regex &pattern){
 
 int main()
 {
-    std::string data = "iefhe uifhe ufeh 19:23:23 20:01:01 iefnh 2a:32:12 2002.02.03 ife 200j.02.wi uefh uefh uefh efwuhiuefhui 37273gr64g2376g 9999.13.12 9999.12.01";
+    std::string data = "fr rg2000.12.1 ufhn 9999.12.31 2000.12.12 2000.12.1a 01:59:59 23:59:59 00:12:60 00:12:23 eufh 21:12:23 uefh 1000.13.31 eifj iehf uwhd uwdh uwqdh uh 1231.09.31 uwdh uwdh uwdbh wudh 12jan.12.53 2000.12.aa";
     //std::getline(std::cin, data);
 
-    std::regex pattern_date1(R"(\d{4}.[0]{1}[1-9]{1}.[0-2]{1}\d{1})");
-    std::regex pattern_date2(R"(\d{4}.[1]{1}[0-2]{1}.[0-2]{1}\d{1})");
-    std::regex pattern_time1(R"([0-1]{1}\d{1}:[0-5]{1}\d{1}:[0-5]{1}\d{1})");
-    std::regex pattern_time2(R"([2]{1}[0 - 3]{1}:[0-5]{1}\d{1}:[0-5]{1}\d{1})");
-    check(data, pattern_date1);
-    check(data, pattern_date2);
-    check(data, pattern_time1);
-    check(data, pattern_time2);
+    std::regex pattern_date(R"((\d{4}).(0[1-9]{1}|1[0-2]{1}).([0-2]{1}[0-9]{1}|31|30))");
+    std::regex pattern_time(R"(([0-1]{1}\d{1}|2[0-3]{1}):([0-5]{1}\d{1}):([0-5]{1}\d{1}))");
+    check(data, pattern_date);
+    check(data, pattern_time);
 
     return 0;
 }
